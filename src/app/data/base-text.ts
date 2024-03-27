257e834ea974255d3d68e2ce6f7e88233cdfcc46
Elena Swiderskaya <olenyonock7@rambler.ru>
2024-03-27 23:06:25 +0300

    base-text

--- src/app/data/base-text.ts
+++ src/app/data/base-text.ts
@@ -1,4 +1,12 @@
 export const BaseTexts = [
-    {textTitle: "Shakespeare", textContent: "To be, or not to be, that is the question!"},
-    {textTitle: "Tesla China", textContent: "Congratulations Tesla China for amazing execution last year. Now on to the next for even more!"},
-  ];
+  {
+    textTitle: 'Learning languages as a way to expand your life',
+    textContent:
+      'My coworker likes to say: "When I learn a foreign language, I expand my inner world as much as if I lived another life". He knows seven languages, of course, not all perfectly, some - only basic concepts. But his motto perfectly describes the miracle that learning a new language can do for us.',
+  },
+  {
+    textTitle: 'Some thoughts',
+    textContent:
+      'To get anywhere, strike out for somewhere, or you will get nowhere. A dream becomes a goal when action is taken toward its achievement. Even a stopped clock is right twice a day',
+  },
+];
